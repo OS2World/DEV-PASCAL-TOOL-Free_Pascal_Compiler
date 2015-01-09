be36acf34c3ec8b0c84676b97eaaa76fd10a8ca1
martini <miturbide@nextstep.com.ec>
2015-01-09 14:54:26 -0500

    version 1.0.10

---
+++ ugtkemx/examples/gtk/ttt_test.pp
@@ -0,0 +1,53 @@
+{
+  $Id: ttt_test.pp,v 1.2 2002/09/07 15:42:58 peter Exp $
+
+  Converted from C to Pascal by Frank Loemker
+  <floemker@techfak.uni-bielefeld.de>
+}
+program ttt_test;
+uses
+  glib,gdk,gtk,
+  tictactoe;
+
+procedure win (widget : pGtkWidget ; data: gpointer); cdecl;
+begin
+  writeln ('Yay!');
+  tictactoe_clear (pTICTACTOE (widget));
+end;
+
+
+var
+  window,ttt : pGtkWidget;
+begin
+  gtk_init (@argc, @argv);
+
+  window := gtk_window_new (GTK_WINDOW_TOPLEVEL);
+
+  gtk_window_set_title (pGTKWINDOW (window), 'Aspect Frame');
+
+  gtk_signal_connect (pGTKOBJECT (window), 'destroy',
+                      GTK_SIGNAL_FUNC (@gtk_exit), NIL);
+
+  gtk_container_set_border_width (pGTKCONTAINER (window), 10);
+
+  ttt := tictactoe_new ();
+
+  gtk_container_add (pGTKCONTAINER (window), ttt);
+  gtk_widget_show (ttt);
+
+  gtk_signal_connect (pGTKOBJECT (ttt), 'tictactoe',
+                      GTK_SIGNAL_FUNC (@win), NIL);
+
+  gtk_widget_show (window);
+
+  gtk_main ();
+end.
+{
+  $Log: ttt_test.pp,v $
+  Revision 1.2  2002/09/07 15:42:58  peter
+    * old logs removed and tabs fixed
+
+  Revision 1.1  2002/01/29 17:55:06  peter
+    * splitted to base and extra
+
+}
